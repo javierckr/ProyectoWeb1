a081d3a9b936f9d43ff2fd5049ec0f30c5e7f43f
javierckr <holajao23@gmail.com>
2021-01-02 14:42:31 +0100

    Funciones de JavaScript añadidas

--- JS/principal.js
+++ JS/principal.js
@@ -1,12 +1,17 @@
 // Función para calcular la edad
+// Badada en w3resource.com
 function calculaEdad(FechaNacimiento){
-    // Guarda la fecha de hoy en la variable hoy
-    var hoy = new Date();
+    // Conviete la fecha al formato de fecha
+    var FechaObj = new Date(FechaNacimiento);
+    // Guarda la diferencia de Milisegundos entre hoy y la fecha de nacimiento
+    var diff_ms = Date.now() - FechaObj.getTime();
+    // Convertimos la diferencia a una fecha, esto nos va a devolver la edad mas 1970, luego se lo restaremos
+    var age_dt = new Date(diff_ms);
     // Si Tiene más de 18...
-    return (hoy.getFullYear()-FechaNacimiento) >= 18 ? true : false;
+    //        console.log(Math.abs(age_dt.getUTCFullYear() - 1970));
+    return (Math.abs(age_dt.getUTCFullYear() - 1970) >= 18) ? true : false;
 }
-
-//Función para validar dnis
+//Función para validar dni
 function dniValidate(DNI){
     // Array para guardar las letras del dni con el número que le corresponde, es la posición
     var arrControl = ["t","r","w","a","g","m","y","f","p","d","x","b","n","j","z","s","q","v","h","l","c","k","e"];
@@ -17,18 +22,36 @@ function dniValidate(DNI){
     // Si la posicion del resto en el array control coincide con la letra devuelve true si no false
     return (arrControl[resto] == letra) ? true : false ;
 }
+//Procedimiento cuando se pulsa el botón
+function formSubmited(){
+    //Llamadas a las funciones para comprobar Dni y fecha nacimiento, se llama primero a la de la edad, porque consume menos recursos
+    if(calculaEdad(document.getElementById("age").value)){
+        //console.log("Mayor de edad");
+        //Ahora comprobaremos que el dni es correcto
+        var DNI = document.getElementById("dni").value;
+        // Quitar espacios del principio y del final por si se ha equivocado
+        DNI = DNI.trim();
+        // Si el numero de caracteres es válido
+        if (DNI.length == 9){
+            // Llama a la función para validar el DNI
+            // Condición en función de si se ha validado o no
+            if(dniValidate(DNI.toLowerCase())){
+                // Si todo es correcto se crea una galletita con el nombre de usuario
+                // Que se borra al cerrar el navegador, para facilitar pruebas...
+                document.cookie = document.getElementById("nom").value;
+            }else{
+                alert("DNI no válido");
+            }
+        }else{
+            console.log("El DNI no es válido\n");
 
-var DNI = prompt("Introduzca DNI", "Harry Potter");
-// Quitar espacios del principio y del final por si se ha equivocado
-DNI = DNI.trim();
-// Si el numero de caracteres es válido
-if (DNI.length == 9){
-    // Llama a la función para validar el DNI
-    var Validated = dniValidate(DNI.toLowerCase());
-    // Condición en función de si se ha validado o no
-    Validated ? console.log("El DNI es válido \n") : console.log("El DNI no es válido\n");
-}else{
-    console.log("El DNI no es válido\n");
-
+        }
+    }else{
+        alert("No es mayor de edad");
+    }
 }
 
+
+
+// Event listener para cuando se evia el formulario
+document.getElementById("submit").addEventListener("submit", formSubmited);
